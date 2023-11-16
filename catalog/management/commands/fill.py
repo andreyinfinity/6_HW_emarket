86d11a3ae0c0f1d60f2ae06cf1b678591e3e6dae
Andrey Potapov <aapot@ya.ru>
2023-11-16 17:30:20 +0300

    6 задание

---
+++ catalog/management/commands/fill.py
@@ -0,0 +1,11 @@
+import os
+
+from django.core.management import BaseCommand
+from catalog.models import Category, Product
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        Category.objects.all().delete()
+        Product.objects.all().delete()
+        return os.system("python manage.py loaddata catalog_data.json")
